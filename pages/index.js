a7a425cb929934a13be676da1d6348316c9310d5
Nick Selvaggio <nick@segosolutions.com>
2022-09-20 12:53:11 -0400

    Allow users to select events and the HTTP endpoint to post to

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { useState } from "react";
 const Index = () => {
 
   const [contractAddress, setContractAddress] = useState('');
+  const [events, setEvents] = useState([]);
+  const [url, setURL] = useState('');
+  const [selectedEvent, setSelectedEvent] = useState('');
 
   const submitContractAddress = (e) => {
     e.preventDefault();
@@ -20,18 +23,56 @@ const Index = () => {
 
       // Filter the abi for event types
       const events = abi.filter(item => item.type === 'event');
-      console.log(events);
+      setEvents(events);
     }
     ).catch(err => {
       console.log(err);
     });
   }
 
+  const createListener = (e) => {
+    e.preventDefault();
+
+    console.log("We are going to create a listener for this event");
+    console.log(events[selectedEvent]);
+
+    // @TODO: Build a object to send to the backend and register a listener.
+  }
+
   return (
-    <div>
+    <div className="grid grid-cols-3">
       <div className="flex flex-col items-center justify-center h-screen">
         <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-1/3 appearance-none leading-normal" type="text" placeholder="Contract Address" onChange={(e) => setContractAddress(e.target.value)} />
         <button onClick={submitContractAddress} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Next</button>
+      </div>
+      <div className="grid m-10">
+        {events.length > 0 && <h1 className="font-bold text-2xl col-span-2">Events Available</h1>}
+        {events.length > 0 && events.map((event, index) => {
+          return (
+            <div key={index} className="border-solid rounded-lg border-4 p-5 m-2">
+              <input type="radio" name="event" onChange={(e) => setSelectedEvent(e.target.value)} value={index} />
+              <h3 className="font-bold">{event.name}</h3>
+              <p>
+                {event.inputs.map((input, index) => {
+                  return (
+                    <span key={index}>{input.name} - {input.type}</span>
+                  )
+                })}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        {events.length > 0 &&
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="font-bold text-2xl">Event Action</div>
+          <p>What to do when we hear an event?</p>
+          <label className="block text-grey-700 text-sm font-bold mb-2" htmlFor="url"/>
+          <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-1/3 appearance-none leading-normal" type="text" placeholder="URL" onChange={(e) => setURL(e.target.value)} />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" onClick={(e) => createListener(e)}>Start Listening</button>
+        </div>
+        }
       </div>
     </div>
   );
