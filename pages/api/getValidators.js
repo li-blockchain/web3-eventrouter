94a51f5490fbf35e934d7fc7f7f837054d92e4c3
Nick Selvaggio <nick@segosolutions.com>
2023-05-31 10:59:14 -0400

    Fix some logic errors in the data filtering

--- pages/api/getValidators.js
+++ pages/api/getValidators.js
@@ -3,7 +3,7 @@
 import { collection, getDocs, getFirestore, query } from "firebase/firestore";
 import app from "../../firebase/clientApp";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     // Read from the firebase database
     const db = getFirestore(app);
     const collRef = collection(db, "rewards_new");
@@ -11,49 +11,48 @@ export default function handler(req, res) {
     // Get from / to dates from the request
     const { startDatetime, endDatetime } = req.query;
 
-    const q = query(collRef);
+    const q = await query(collRef);
+
+    const rewards = [];     // Rewards are currently indexed by epoch in our database.
+    const validators = {};
 
     getDocs(q).then((querySnapshot) => {
-        const rewards = [];     // Rewards are currently indexed by epoch in our database.
-        const validators = {};
+
         querySnapshot.forEach((doc) => {
             // spread doc.data with doc.id
             rewards.push({...doc.data(), id: doc.id });
         });
 
         // Loop through all rewards and get the validatorindex
-        rewards.forEach(reward => {
+        rewards.forEach((reward) => {
             // Loop through all withdrawals
-            reward.withdrawals.forEach(withdrawal => {
-                if(withdrawal.datetime <= startDatetime || withdrawal.datetime >= endDatetime) {
-                    return;
-                }
-                // Adding a validator and withdrawals
-                if (!validators[withdrawal.validator_index]) {
-                    validators[withdrawal.validator_index] = {};
-                    validators[withdrawal.validator_index]['withdrawals'] = withdrawal.amount;
-                    validators[withdrawal.validator_index]['proposals'] = 0;
-                } else {
-                    validators[withdrawal.validator_index]['withdrawals'] += withdrawal.amount;
+            reward.withdrawals.forEach((withdrawal) => {            
+                if(withdrawal.datetime >= startDatetime && withdrawal.datetime <= endDatetime) {
+                    // Adding a validator and withdrawals
+                    if (!validators[withdrawal.validator_index]) {
+                        validators[withdrawal.validator_index] = {};
+                        validators[withdrawal.validator_index]['withdrawals'] = withdrawal.amount;
+                        validators[withdrawal.validator_index]['proposals'] = 0;
+                    } else {
+                        validators[withdrawal.validator_index]['withdrawals'] += withdrawal.amount;
+                    }
                 }
             });
 
             reward.proposals.forEach(data => {
-                if(data.datetime <= startDatetime || data.datetime >= endDatetime) {
-                    return;
-                }
-                // Adding a validator and withdrawals
-                if (!validators[data.validator_index]) {
-                    validators[data.validator_index] = {};
-                    validators[withdrawal.validator_index]['withdrawals'] = 0;
-                    validators[data.validator_index]['proposals'] = parseInt(data.amount);
-                } else {
-                    validators[data.validator_index]['proposals'] += parseInt(data.amount);
+                if(data.datetime >= startDatetime && data.datetime <= endDatetime) {
+                    // Adding a validator and withdrawals
+                    if (!validators[data.validator_index]) {
+                        validators[data.validator_index] = {};
+                        validators[withdrawal.validator_index]['withdrawals'] = 0;
+                        validators[data.validator_index]['proposals'] = parseInt(data.amount);
+                    } else {
+                        validators[data.validator_index]['proposals'] += parseInt(data.amount);
+                    }
                 }
             });
         });
-
         res.status(200).json(validators);
     });
-    
+
 }
