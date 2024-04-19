96dab0967255e470c3176f58f687273df2b48aac
Nick Selvaggio <nick@segosolutions.com>
2024-04-19 16:58:08 -0400

    Added ability login with enter, display basic errors

--- pages/user/index.js
+++ pages/user/index.js
@@ -9,49 +9,57 @@ import Link from 'next/link';
 
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const auth = getAuth(app);
-    const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
-    const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth(app);
+  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState('');
 
-    useEffect(() => {
-        if(user && !loading) {
-            router.push('/');
-        }
-    }, [user, loading]);
-
-    if(error) {
-        console.log(error);
+  useEffect(() => {
+    if (user && !loading) {
+      router.push('/');
     }
 
-    if(loading) {
-        console.log('loading');
+    if (error) {
+      // Replaces 'Firebase' with an empty string or any other placeholder text you prefer
+      const filteredError = error.message.replace(/Firebase:/gi, '');
+      setErrorMessage(filteredError);
     }
+  }, [user, loading, error]);
 
-    return (
-        <div className="grid h-screen place-items-center bg-slate-200">
-            <div className="grid w-1/2 p-10 bg-white rounded-lg shadow-xl">
-                <img
-                      className="h-10 w-auto"
-                      src="/logo.png"
-                      alt="Crypto Creator Kit"
-                    />
-                <h1 className="text-3xl py-2 font-bold">Login</h1>
-                <div className="p-3">
-                    <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="p-3">
-                    <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div className="p-3">
-                    <button onClick={() => signInWithEmailAndPassword(email, password)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Login</button>
-                </div>
-                {/* <div>
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      signInWithEmailAndPassword(email, password);
+    }}>
+
+      <div className="grid h-screen place-items-center bg-slate-200">
+        <div className="grid w-1/2 p-10 bg-white rounded-lg shadow-xl">
+          <img
+            className="h-10 w-auto"
+            src="/logo.png"
+            alt="Long Island Blockchain Client Login"
+          />
+          <h1 className="text-3xl py-2 font-bold">Login</h1>
+          <div className="p-3">
+            <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="p-3">
+            <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div className="p-3 text-red-500">
+            {errorMessage && <p>{errorMessage}</p>}
+          </div>
+          <div className="p-3">
+            <button onClick={() => signInWithEmailAndPassword(email, password)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Login</button>
+          </div>
+          {/* <div>
                     New Here? <Link href="/user/new"><a className="text-blue-600">Create an account</a></Link>
                 </div> */}
-            </div>        
         </div>
-    )
+      </div>
+    </form>
+  )
 
 }
